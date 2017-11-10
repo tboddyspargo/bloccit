88562cb0066e952b2ed10b59003fe8cacd9628d5
Tyler BoddySpargo <muyleche@gmail.com>
2017-11-10 01:07:46 +0000

    Checkpoint 21 - Topics and Posts: completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  let(:post) { Post.create!(title: "New Post Title", body: "New post body.") }
-    let(:comment) { Comment.create(body: "Comment body.", post: post) }
-    
-    describe "attributes" do
-        it "has body attribute" do
-            expect(comment).to have_attributes(body: "Comment body.") 
-        end
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+  let(:post) { topic.posts.create!(title: "New Post Title", body: "New post body.") }
+  let(:comment) { Comment.create(body: "Comment body.", post: post) }
+  
+  describe "attributes" do
+    it "has body attribute" do
+      expect(comment).to have_attributes(body: "Comment body.") 
     end
+  end
 end
