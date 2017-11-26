6c838dd52b84c55bcbd085904e65b596c88d3ba1
Tyler BoddySpargo <muyleche@gmail.com>
2017-11-26 20:42:03 +0000

    checkpoint 27 - Posts and Users: complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ActiveRecord::Base
     has_many :comments, dependent: :destroy
     belongs_to :topic
+    belongs_to :user
+    
+    default_scope { order('created_at DESC') }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 require 'random_data'
 
+5.times do
+  User.create!(
+    name: RandomData.random_name,
+    email: RandomData.random_email,
+    password: RandomData.random_sentence
+  )
+end
+
+users = User.all
+user = users.first
+user.update_attributes!(
+  email: 'muyleche@gmail.com',
+  password: 'p@ssw0rd'
+)
+
 15.times do
   Topic.create!(
     name: RandomData.random_sentence,
@@ -10,6 +25,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -27,6 +43,7 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
