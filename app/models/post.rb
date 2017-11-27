563d7331afe2897b83444230ad74d695b60bb978
Tyler BoddySpargo <muyleche@gmail.com>
2017-11-27 01:43:48 +0000

    assignment 27 - posts and users: complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ActiveRecord::Base
     belongs_to :user
     
     default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { order('title ASC') }
+    scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
     
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
