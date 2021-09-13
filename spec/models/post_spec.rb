de895584f05eba4e49e4d9fa5bff0b71ac0fdc31
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-13 18:44:28 -0500

    test(factories): to comply with factory_bot and changes in usage of shoulda.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,22 +4,25 @@ RSpec.describe Post, type: :model do
   let(:topic) { create(:topic) }
   let(:user) { create(:user) }
   let(:post) { create(:post) }
-  
-  it { is_expected.to belong_to(:topic) }
-  it { is_expected.to belong_to(:user) }
-  
-  it { is_expected.to have_many(:comments) }
-  it { is_expected.to have_many(:votes) }
-  it { is_expected.to have_many(:favorites) }
-  
-  it { is_expected.to validate_presence_of(:title) }
-  it { is_expected.to validate_presence_of(:body) }
-  it { is_expected.to validate_presence_of(:topic) }
-  it { is_expected.to validate_presence_of(:user) }
-  
-  it { is_expected.to validate_length_of(:title).is_at_least(5) }
-  it { is_expected.to validate_length_of(:body).is_at_least(20) }
-  
+
+  describe "validations" do
+    
+    it { should belong_to(:topic) }
+    it { should belong_to(:user) }
+    
+    it { should have_many(:comments) }
+    it { should have_many(:votes) }
+    it { should have_many(:favorites) }
+    
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:topic) }
+    it { should validate_presence_of(:user) }
+    
+    it { should validate_length_of(:title).is_at_least(5) }
+    it { should validate_length_of(:body).is_at_least(20) }
+  end
+
   describe "attributes" do
     it "has title and body attributes" do
         expect(post).to have_attributes(title: post.title, body: post.body, user: post.user)
