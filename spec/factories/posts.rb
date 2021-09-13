a7786a15faab98e9262dfb7689cd6992b15f398e
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-13 18:44:28 -0500

    test(factories): to comply with factory_bot and changes in usage of shoulda.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :post do
-    title RandomData.random_sentence
-    body RandomData.random_paragraph
+    title { RandomData.random_sentence }
+    body { RandomData.random_paragraph }
     topic
     user
-    rank 0.0
+    rank { 0.0 }
   end
 end
--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :topic do
-    name RandomData.random_name
-    description RandomData.random_sentence
+    name { RandomData.random_name }
+    description { RandomData.random_sentence }
   end
 end
