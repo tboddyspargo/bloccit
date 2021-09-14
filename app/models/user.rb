a45eda510e6d259ab5fc596f8c18f60b974c05ab
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-14 10:56:31 -0500

    fix(): address code changes requried for rails 5.0 and set config defaults

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
