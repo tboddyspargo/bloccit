b2a11f19806f21d0ea32bff2f7fda0b5f868106b
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-14 10:57:55 -0500

    test(): use responders to make sure controllers declare the request types they respond_to; fix session controller to get attributes from params directly; address deprecation warnings for http request methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
-  
+  respond_to :html, :json
   def new
   end
   
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:email].downcase)
     
-    if user && user.authenticate(params[:session][:password])
-      create_session(user)
-      flash[:notice] = "Welcome, #{user.name}!"
-      redirect_to root_path
-    else
-      flash.now[:alert] = "Invalid email/password combination."
-      render :new
+    respond_with(user) do |format|
+      if user && user.authenticate(params[:password])
+        create_session(user)
+        format.html { redirect_to root_path, notice: "Welcome, #{user.name}!" }
+      else
+        flash.now[:alert] = "Invalid email/password combination."
+        format.html { render :new }
+      end
     end
   end
   
   def destroy
     destroy_session(current_user)
-    flash[:notice] = "You've been signed out, come back soon!"
-    redirect_to root_path
+    redirect_to root_path, notice: "You've been signed out, come back soon!"
   end
 end
