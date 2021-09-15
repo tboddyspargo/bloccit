7d8da054f9579b328d0acf246335b14ae3003757
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-14 19:58:11 -0500

    build(): update to Rails 5.2.latest, run app:update, make necessary code changes to pass tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   
   validates :name, length: { minimum: 1 }, presence: true
   validates :email, length: { minimum: 3, maximum: 254 }, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }, presence: true, if: "password_digest.nil?"
+  validates :password, length: { minimum: 6 }, presence: true, if: -> { password_digest.nil? }
   validates :password, length: { minimum: 6 }, allow_blank: true
   
   has_secure_password
