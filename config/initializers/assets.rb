69392538d5ddabb4f7cd565cb7c70ca853672b85
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-15 18:44:57 -0500

    chore(): use Rails 6 feature flags; prevent rails g from creating js files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,5 @@
 
 # Add node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
+
+Rails.application.config.generators.javascripts = false
