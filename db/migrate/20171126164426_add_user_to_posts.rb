f1782fa8ab30a9cf9a335cd11e0387c508e1df25
Tyler BoddySpargo <muyleche@gmail.com>
2021-09-15 18:49:19 -0500

    fix(): Rails changes the Migrations API so we need to specify the version a migration was created from.

--- db/migrate/20171126164426_add_user_to_posts.rb
+++ db/migrate/20171126164426_add_user_to_posts.rb
@@ -1,4 +1,4 @@
-class AddUserToPosts < ActiveRecord::Migration
+class AddUserToPosts < ActiveRecord::Migration[4.2]
   def change
     add_column :posts, :user_id, :integer
     add_index :posts, :user_id
